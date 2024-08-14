039c32467c2427caa6d28c1a067b7659443981d4
Chucks <emordichukwuma@gmail.com>
2024-08-14 14:17:59 +0100

    Resolve Dependencies issue

--- Headstarter/Chucks_Pantry/pages/firebase.js
+++ Headstarter/Chucks_Pantry/pages/firebase.js
@@ -2,20 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-require('dotenv').config();
 
-
-import 'firebase/auth';
-import 'firebase/firestore';
-
+// Firebase configuration
 const firebaseConfig = {
- apiKey: "AIzaSyD_YFXBLgT5uYXAKD_Dwex6Pk01r--dFlU",
-  authDomain: "pantry-headstarter-13b56.firebaseapp.com",
-  projectId: "pantry-headstarter-13b56",
-  storageBucket: "pantry-headstarter-13b56.appspot.com",
-  messagingSenderId: "920193893272",
-  appId: "1:920193893272:web:815807685b231fa20561ed",
-  measurementId: "G-10FWPBNR0V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -25,5 +21,5 @@ export const auth = getAuth(app);
 
 // Initialize Firebase Analytics (for browser only)
 if (typeof window !== 'undefined') {
-    const analytics = getAnalytics(app);
+  const analytics = getAnalytics(app);
 }
